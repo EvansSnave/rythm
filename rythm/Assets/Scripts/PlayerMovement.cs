af448bc4193495bcf02e1c8e4350d390f0c88a68
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-09-17 08:32:28 -0500

    Create jumpoing system

---
+++ rythm/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+	public float jumpSpeed;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask groundLayer;
+
+	private bool _isGrounded;
+	private Rigidbody2D _body;
+
+	private void Awake()
+	{
+		_body = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		_isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+		_body.velocity = new (Input.GetAxis("Horizontal") * speed, _body.velocity.y);
+
+		FlipCharacterOnMovement();
+
+		if (Input.GetKey(KeyCode.Space) && _isGrounded) _body.velocity = new (_body.velocity.x, jumpSpeed);
+	}
+
+	private void FlipCharacterOnMovement()
+	{
+		float _horizontalAxis = Input.GetAxis("Horizontal");
+
+		if (_horizontalAxis > 0.01f) transform.localScale = Vector3.one;
+		else if (_horizontalAxis < -0.01f) transform.localScale = new(-1, 1, 1);
+	}
+
+}
