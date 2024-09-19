b2d3126ec8a21496694fd8999445513360aad26a
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-09-19 11:06:50 -0500

    Create tweakable rhythm screen

---
+++ rythm/Assets/Scripts/Rhythm.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Rhythm : MonoBehaviour
+{
+	private Image _rhythmGuide;
+	[SerializeField] private float _fadeDuration = 1.0f;
+	[SerializeField] private float _tempoDuration = 4.0f;
+	[SerializeField] private float[] _beatTimings;
+
+	private void Awake()
+	{
+		_rhythmGuide = GetComponent<Image>();
+
+		if (_rhythmGuide == null)
+		{
+			Debug.LogError("No Image component found on the GameObject!");
+		}
+	}
+
+	private void Start()
+	{
+		StartCoroutine(RhythmLoop());
+	}
+
+	private IEnumerator RhythmLoop()
+	{
+		while(true)
+		{
+			foreach(float beatTime in _beatTimings) {
+				Invoke("Beat", beatTime);
+			}
+
+			yield return new WaitForSeconds(_tempoDuration);
+		}
+	}
+
+	private void Beat()
+	{
+		if (_rhythmGuide == null) return;
+
+		Color imageColor = _rhythmGuide.color;
+		imageColor.a = 1;
+		_rhythmGuide.color = imageColor;
+
+		StartCoroutine(FadeOut());
+	}
+
+	private IEnumerator FadeOut()
+	{
+		float elapsedTime = 0f;
+		Color startColor = _rhythmGuide.color;
+		Color endColor = new(startColor.r, startColor.g, startColor.b, 0);
+
+		while (elapsedTime < _fadeDuration)
+		{
+			elapsedTime += Time.deltaTime;
+			_rhythmGuide.color = Color.Lerp(startColor, endColor, elapsedTime / _fadeDuration);
+			yield return null;
+		}
+
+		_rhythmGuide.color = endColor;
+	}
+}
